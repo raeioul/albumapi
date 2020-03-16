af74d409192af062b27570996ae490560901128c
Raúl Acuña <raeioul@gmail.com>
2020-03-16 08:41:26 -0400

    Little refactor to avoid hardcoding

--- src/constants/albumApi.constants.ts
+++ src/constants/albumApi.constants.ts
@@ -1,1 +1,2 @@
 export const PORT = 9001;
+export const WELCOME_MESSAGE = "Welcome to albumAPI REST by raeiouL";
--- src/services/album.service.ts
+++ src/services/album.service.ts
@@ -1,7 +1,8 @@
 import { Request, Response } from "express";
+import { WELCOME_MESSAGE } from "../constants/albumApi.constants";
 
 export class AlbumService {
   public welcomeMessage(req: Request, res: Response) {
-    return res.status(200).send("Welcome to albumAPI REST by raeiouL");
+    return res.status(200).send(WELCOME_MESSAGE);
   }
 }
