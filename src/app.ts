637014c11a91010311925e04c57e047d39863ddb
Raúl Acuña <raeioul@gmail.com>
2020-03-16 09:44:42 -0400

    Add mongo db to app.ts

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,9 @@
-import express, { Application } from "express";
-import { Controller } from "./main.controller";
-import bodyParser from "body-parser";
-import cors from "cors";
+import express, { Application } from 'express';
+import { Controller } from './main.controller';
+import { MONGO_URL } from './constants/albumApi.constants';
+import bodyParser from 'body-parser';
+import cors from 'cors';
+import mongoose from 'mongoose';
 
 class App {
   public app: Application;
@@ -10,13 +12,21 @@ class App {
   constructor() {
     this.app = express();
     this.setConfig();
+    this.setMongoConfig();
     this.albumController = new Controller(this.app);
   }
 
   private setConfig() {
-    this.app.use(bodyParser.json({ limit: "50mb" }));
-    this.app.use(bodyParser.urlencoded({ limit: "50mb", extended: true }));
+    this.app.use(bodyParser.json({ limit: '50mb' }));
+    this.app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
     this.app.use(cors());
+  }
+
+  private setMongoConfig() {
+    mongoose.Promise = global.Promise;
+    mongoose.connect(MONGO_URL, {
+      useNewUrlParser: true
+    });
   }
 }
 
